687d6be818c61789302e8ce9e0e1680d7db1af30
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-09-26 12:46:02 +0100

    add comments

--- Session_10/Task_1.2/BMI.js
+++ Session_10/Task_1.2/BMI.js
@@ -1,16 +1,18 @@
+// identifies the submit button
 const submitButton = document.getElementById("submit")
 
 
-
+//establishes the click event for the button
 submitButton.onclick = function() {
+    // stores user height input
     const userHeight = document.getElementById("height").value;
-    console.log(userHeight);
+    // stores user weight input
     const userWeight = document.getElementById("weight").value;
-    console.log(userWeight);
+    // standard BMI calculation
     const bmi = function(w, h) {
         let heightSquare = h * h;
         return w / heightSquare;
     }
+    // changes <p> element to display BMI based on user inputs
     document.getElementById("output").innerHTML = bmi(userWeight, userHeight);
 };
-
