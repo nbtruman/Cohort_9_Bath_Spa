aa27a6b571283f94c3055ec550e765f9926a5579
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-09-26 15:07:49 +0100

    changes

--- Session_10/Task_1.3/toggle.js
+++ Session_10/Task_1.3/toggle.js
@@ -1,17 +1,21 @@
+// stores elements as variables
 const toggle = document.getElementById("toggle");
 const bulb = document.getElementById("bulb");
-const on = "./pic_bulbon.gif"
-const off = "./pic_bulboff.gif";
-let counter = 0
+const bulbOn = "./pic_bulbon.gif"
+const bulbOff = "./pic_bulboff.gif";
+const switchOff = "./light_switch_off.png";
+const switchOn = "./light_switch_on.png";
 
+let counter = 0
+// very ugly way to make it switch based on a binary choice. i couldn't make the ternary operator work.
 toggle.onclick = function() {
     counter += 1;
     console.log(counter)
     if(Number.isInteger(counter/2)){
-        bulb.src = off;
-        toggle.src = "./light_switch_off.png";
+        bulb.src = bulbOff;
+        toggle.src = switchOff;
     }else{
-        bulb.src = on;
-        toggle.src= "./light_switch_on.png";
+        bulb.src = bulbOn;
+        toggle.src = switchOn;
     }
 }
