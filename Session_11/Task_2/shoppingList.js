1a2ea36e579422c732d007490225cdf70e572476
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-09-27 13:15:03 +0100

    add clear button

--- Session_11/Task_2/shoppingList.js
+++ Session_11/Task_2/shoppingList.js
@@ -2,6 +2,7 @@ const list = [];
 const shoppingList = document.getElementById("shoppingList")
 const input = document.getElementById("item");
 const add = document.getElementById("add");
+const clear = document.getElementById("clear");
 
 add.onclick = function addItem() {
     const newItem = input.value
@@ -13,9 +14,18 @@ add.onclick = function addItem() {
         list.push(newItem)        
         input.value = ""
         console.log(list)
+        
+        const newLine = document.createElement("li");
+        newLine.innerHTML = newItem;
+        shoppingList.appendChild(newLine);
     }
-    const newLine = document.createElement("li");
-    newLine.innerHTML = newItem;
-    shoppingList.appendChild(newLine);
     
+}
+
+clear.onclick = function clearList() {
+    shoppingList.innerHTML = ""
+    list.forEach(i => {
+        list.pop();
+        console.log(list)
+    })
 }
