ce80f408e152885a159842859b7f647cb9c6d5f3
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-09-27 13:38:54 +0100

    clear box on alert functionality

--- Session_11/Task_2/shoppingList.js
+++ Session_11/Task_2/shoppingList.js
@@ -7,13 +7,15 @@ const clear = document.getElementById("clear");
 add.onclick = function addItem() {
     const newItem = input.value
     if(list.includes(newItem)) {
-        alert("That item has already been added");
+        alert("That item has already been added");        
+        input.value = "";
     }else if(newItem == ""){
-        alert("Please type an item to add")
+        alert("Please type an item to add");        
+        input.value = "";
     }else{        
-        list.push(newItem)        
-        input.value = ""
-        console.log(list)
+        list.push(newItem);        
+        input.value = "";
+        console.log(list);
         
         const newLine = document.createElement("li");
         newLine.innerHTML = newItem;
