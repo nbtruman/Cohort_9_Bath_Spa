eb948fdb7c76ec42083efa847eb9d90a9d9dc874
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-10-06 13:56:09 +0100

    change variable names for clarity

--- Session_17/interactive-rating-component-main/app.js
+++ Session_17/interactive-rating-component-main/app.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("myForm");
-const rating = document.getElementById("rating");
+const ratingCard = document.getElementById("rating");
 const submission = document.getElementById("submission");
 const ratingOutput = document.getElementById("ratingOutput");
 
@@ -7,7 +7,7 @@ form.addEventListener("submit", (e)=> {
     e.preventDefault();
     const output = form.rating.value;
     console.log(output);
-    rating.style.display = "none";
+    ratingCard.style.display = "none";
     ratingOutput.innerHTML = `You selected ${output} out of 5`
     submission.style.display = "flex";
 })
